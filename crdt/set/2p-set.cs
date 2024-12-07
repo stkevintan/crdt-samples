2a7f67ea3afdd6786563ced3d8810f8c465364c5
stkevintan <stkevintan@zju.edu.cn>
2024-12-07 09:01:04 +0800

    first

---
+++ crdt/set/2p-set.cs
@@ -0,0 +1,28 @@
+namespace CRDT.Set {
+    class TwoPSet<T> {
+        private readonly GSet<T> _addSet = new();
+
+        private readonly GSet<T> _removeSet = new();
+
+        public void Add(T element) {
+            _addSet.Add(element);
+        }
+
+        public void Remove(T element) {
+            _removeSet.Add(element);
+        }
+
+        public bool Contains(T element) {
+            return _addSet.Contains(element) && !_removeSet.Contains(element);
+        }
+
+        public void Merge(TwoPSet<T> other) {
+            _addSet.Merge(other._addSet);
+            _removeSet.Merge(other._removeSet);
+        }
+
+        public IEnumerable<T> Query() {
+            return _addSet.Query().Where(e => !_removeSet.Contains(e));
+        }
+    }
+}
---
+++ crdt/set/g-set.cs
@@ -0,0 +1,27 @@
+namespace CRDT.Set
+{
+    public class GSet<T>
+    {
+        private readonly HashSet<T> _set = [];
+
+        public void Add(T element)
+        {
+            _set.Add(element);
+        }
+
+        public bool Contains(T element)
+        {
+            return _set.Contains(element);
+        }
+
+        public void Merge(GSet<T> other)
+        {
+            _set.UnionWith(other._set);
+        }
+
+        public IEnumerable<T> Query()
+        {
+            return _set;
+        }
+    }
+}
